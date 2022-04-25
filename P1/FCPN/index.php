4ec67c1cffd6ab390ef3a321a05470d626449c1f
G00F7 <104342529+G00F7@users.noreply.github.com>
2022-04-25 02:00:34 -0400

    Add files via upload

---
+++ P1/FCPN/index.php
@@ -0,0 +1,109 @@
+<?php include "index-inicio.php";?>
+<!-- Services -->
+<div class="services section-padding bg-grey" data-scroll-index='1'>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 section-title text-center">
+        <h3>Facultad de Ciencias Puras y Naturales</h3>
+        <p>Algunos aspectos que ofrece la facultad de ciencias puras y naturales.</p>
+        <span class="section-title-line"></span> </div>
+      <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb-30">
+        <div class="service-box bg-white text-center">
+          <div class="icon"> <i class="fas fa-user-edit"></i> </div>
+          <div class="icon-text">
+            <h4 class="title-box">Prefacultativo</h4>
+            <p>El Curso Pre universitario tiene como objetivo, nivelar y reforzar los conocimientos necesarios en áreas que exige la carrera de interés, existen dos etapas para tomar estos cursos: Prueba de suficiencia académica y Curso Preuniversitario.</p>
+          </div>
+        </div>
+      </div>
+      <i class=""></i>
+      <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb-30">
+        <div class="service-box bg-white text-center">
+          <div class="icon"> <i class="fas fa-user-graduate "></i> </div>
+          <div class="icon-text">
+            <h4 class="title-box">licenciatura</h4>
+            <p>EL objetivo principal es la formación de profesionales de alta calidad, con conocimientos, destrezas y habilidades que hacen posible su inserción creativa en la sociedad, preparados para impulsar y promover la innovación científica y tecnológica.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb-30">
+        <div class="service-box bg-white text-center">
+          <div class="icon"> <i class="fas fa-chalkboard-teacher"></i> </div>
+          <div class="icon-text">
+            <h4 class="title-box">Maestria</h4>
+            <p>La maestría en ciencias tiene como finalidad la de otorgar al graduado las condiciones para desarrollar estudios que demuestren el dominio de los instrumentos conceptuales y metodológicos esenciales en el área de formación académica.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Services --> 
+<!--Carreras-->
+<!--Fisica-->
+<div class="about-us section-padding" data-scroll-index='2'>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 section-title text-center">
+        <h3>carrera de física</h3>
+        <span class="section-title-line"></span> </div>
+      <div class="col-md-6 mb-50">
+        <div class="section-info">
+          <div class="sub-title-paragraph">
+            <h4>Carrera de Física</h4>
+            <h5>Misión</h5>
+            <p>La Carrera de Física comparte sustancialmente la misión, visión y objetivos de la Universidad Boliviana, los de la UMSA y los de la FCPN. No obstante, tiene como misión propia y fundamental la de formar y entrenar recursos humanos de alto nivel, especializados en la investigación científica, la docencia y la aplicación de conocimientos en todas las áreas de la física.</p>
+          </div>
+          <a href="./Carreras/Física/index.php" class="anchor-btn" >Saber más <i class="fas fa-arrow-right pd-lt-10"></i></a> </div>
+      </div>
+      <div class="col-md-6 mb-50">
+        <div class="section-img"> <img src="images/img-fisica.jpg" alt="" class="img-responsive"/> </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!--Informatica-->
+<div class="about-us section-padding" data-scroll-index='3'>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 section-title text-center">
+        <h3>carrera de informática</h3>
+        <span class="section-title-line"></span> </div>
+      <div class="col-md-6 mb-50">
+        <div class="section-info">
+          <div class="sub-title-paragraph">
+            <h4>Carrera de Informática</h4>
+            <h5>Misión</h5>
+            <p>Desarrollar procesos académicos de formación, profesionalización e investigación en ciencia y tecnología informática de modo que los profesionales informáticos de la Carrera de Informática de la UMSA serán profesionales competitivos, éticos y responsables con altos niveles de conocimiento científico y tecnológico en informática, acorde con los requerimientos actuales y futuros tanto a nivel regional, nacional e internacional.</p>
+          </div>
+          <a href="./Carreras/Informática/index.php" class="anchor-btn">Saber más <i class="fas fa-arrow-right pd-lt-10"></i></a> </div>
+      </div>
+      <div class="col-md-6 mb-50">
+        <div class="section-img"> <img src="images/img-info.jpg" alt="" class="img-responsive"/> </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!--Matematica-->
+<div class="about-us section-padding" data-scroll-index='4'>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 section-title text-center">
+        <h3>carrera de matemática</h3>
+        <span class="section-title-line"></span> </div>
+      <div class="col-md-6 mb-50">
+        <div class="section-info">
+          <div class="sub-title-paragraph">
+            <h4>Carrera de Matemática</h4>
+            <h5>Misión</h5>
+            <p>La Carrera de Matemática de la UMSA, posee un nivel académico de excelencia, forma profesionales e investigadores, comprometidos con la sociedad y sus necesidades; orientados a la creación de conocimientos mediante los enfoques de la Matemática pura y aplicada, que respondan a los retos científicos y tecnológicos de la sociedad, enfatizando en la formación integral orientada al desarrollo y la creación de conocimientos matemáticos en las áreas de la Matemática Pura y Aplicada, para lograr la transformación del país con responsabilidad social y contribuyendo a su desarrollo sostenible.</p>
+          </div>
+          <a href="./Carreras/Matemática/index.php" class="anchor-btn">Saber más <i class="fas fa-arrow-right pd-lt-10"></i></a> </div>
+      </div>
+      <div class="col-md-6 mb-50">
+        <div class="section-img"> <img src="images/img-mate.jpg" alt="" class="img-responsive"/> </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include "index-login.php"; ?>
