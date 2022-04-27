37ca9a4e1c9fc846ad42b2c235900eab4b63b1bf
G00F7 <104342529+G00F7@users.noreply.github.com>
2022-04-27 14:13:10 -0400

    Update index-inicio.php

--- P4/FCPN/index-inicio.php
+++ P4/FCPN/index-inicio.php
@@ -8,7 +8,7 @@
 
 <title><?php echo $materia; ?></title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="style.css"/>
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="css/jquery.fancybox.min.css"/>
 <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css"/>
